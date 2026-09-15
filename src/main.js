52a8f510e729859cf86ff6a5d05ddccc1e3f355f
keyooooo <qiezige777@outlook.com>
2026-09-15 10:49:46 +0800

    bring vite to build frontend

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { initNav } from "../js/nav.js";
+import { initCardsAnim } from "../js/cards.js";
+import { initScoreAnim } from "../js/score.js";
+
+initNav();
+initCardsAnim();
+initScoreAnim();
